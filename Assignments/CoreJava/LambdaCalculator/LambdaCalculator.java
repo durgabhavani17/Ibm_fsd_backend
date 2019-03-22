6181395697eb26d8777506be47269476f1923233
Durga Bhavani Chakali <dchakal1@in.ibm.com>
2019-03-23 01:25:35 +0530

    added

---
+++ Assignments/CoreJava/LambdaCalculator/LambdaCalculator.java
@@ -0,0 +1,58 @@
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+
+public class LambdaCalculator {
+	public static void main(String[] args) {
+		LambdaCalculator m = new LambdaCalculator();
+		BiFunction<Integer, Integer, Integer> addition = (a, b) -> a + b;
+		BiFunction<Integer, Integer, Integer> subtraction = (a, b) -> a - b;
+		BiFunction<Integer, Integer, Integer> multiplication = (a, b) -> a * b;
+		BiFunction<Integer, Integer, Integer> division = (a, b) -> a / b;
+
+		System.out.println("10 + 5 = " + m.operate(10, 5, addition));
+
+		System.out.println("10 - 5 = " + m.operate(10, 5, subtraction));
+
+		System.out.println("10 x 5 = " + m.operate(10, 5, multiplication));
+
+		System.out.println("10 / 5 = " + m.operate(10, 5, division));
+
+		// without parenthesis and using Consumer functional interface
+		
+
+		Consumer<String> greetService1 = message ->
+
+		System.out.println("Hello " + message);
+
+		// with parenthesis and using custom functional interface
+
+		GreetingService greetService2 = (message) ->
+
+		System.out.println("Hello " + message);
+
+		greetService1.accept("Mahesh");
+
+		greetService2.sayMessage("Suresh");
+
+	}
+
+	interface MathOperation {
+
+		int operation(int a, int b);
+
+	}
+
+	interface GreetingService {
+
+		void sayMessage(String message);
+
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+
+	private int operate(int a, int b, BiFunction mathOperation) {
+
+		return (int) mathOperation.apply(a,b);
+
+	}
+}
