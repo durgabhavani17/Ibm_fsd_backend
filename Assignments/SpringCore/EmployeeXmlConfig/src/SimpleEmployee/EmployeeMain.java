4103cb6aa42a68e1d26c8b569ee350f0c6dc68b8
Durga Bhavani Chakali <dchakal1@in.ibm.com>
2019-03-31 00:07:04 +0530

    employee prgram with xml config

---
+++ Assignments/SpringCore/EmployeeXmlConfig/src/SimpleEmployee/EmployeeMain.java
@@ -0,0 +1,26 @@
+package SimpleEmployee;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans_Config.xml");
+		
+		Employee obj = (Employee) context.getBean("employee");
+
+		
+
+		System.out.println(obj.getId());
+		System.out.println(obj.getAddress());
+		System.out.println(obj.getDob());
+		System.out.println(obj);
+
+		// Graceful shutdown of container
+		context.registerShutdownHook();
+
+	}
+
+}
