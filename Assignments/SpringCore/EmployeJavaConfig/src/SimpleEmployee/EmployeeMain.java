5d8d852dc2848369016df8485e98076adacd63ab
Durga Bhavani Chakali <dchakal1@in.ibm.com>
2019-03-30 22:30:57 +0530

    spring core assignments added

---
+++ Assignments/SpringCore/EmployeJavaConfig/src/SimpleEmployee/EmployeeMain.java
@@ -0,0 +1,29 @@
+package SimpleEmployee;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		AbstractApplicationContext context = new AnnotationConfigApplicationContext(EmployeeConfiguration.class);
+      //ApplicationContext
+		// Retrieving the bean from container
+		Employee obj = (Employee) context.getBean("employee");
+		
+
+		// Display message
+		
+		  System.out.println(obj.getId());
+		  System.out.println(obj.getAddress());
+		  System.out.println(obj.getDob()); 
+		  System.out.println(obj);
+		 
+		// Graceful shutdown of container
+		context.registerShutdownHook();
+
+
+	}
+
+}
